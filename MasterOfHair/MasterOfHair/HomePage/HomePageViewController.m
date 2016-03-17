f1ba12394a5b3f218b15eae113b0a71b9362e397
鞠超 <wolaji@wolaji.lan>
2016-03-17 11:48:32 +0800

    修改商盟

--- MasterOfHair/MasterOfHair/HomePage/HomePageViewController.m
+++ MasterOfHair/MasterOfHair/HomePage/HomePageViewController.m
@@ -964,7 +964,7 @@
     if ([dict[@"status"][@"succeed"] intValue] == 1) {
         @try
         {//[@"productlist"]
-            for (NSDictionary * dic in dict[@"data"][@"productlist"])
+            for (NSDictionary * dic in dict[@"data"])
             {
                 WebStroe_Model * model = [[WebStroe_Model alloc] init];
                 
