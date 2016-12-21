42b1b1bc4da2a85cb7503f491b69e10ec8a629c8
于金祥 <1028920761@qq.com>
2016-12-21 10:52:04 +0800

    update

--- MasterOfHair/MasterOfHair/HomePage/RegistViewController.m
+++ MasterOfHair/MasterOfHair/HomePage/RegistViewController.m
@@ -368,7 +368,7 @@
     }
     else
     {
-        [SVProgressHUD dismiss];
+//        [SVProgressHUD dismiss];
         
         [SVProgressHUD showErrorWithStatus:dict[@"status"][@"message"] ];
     }
